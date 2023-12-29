a2b66f5f965003337f93b73aca565b2e25a7b255
bryanjamesl <bryanjamesl@gmail.com>
2023-12-29 15:09:19 -0700

    added another pwm ex

---
+++ pwm-ex-from-rpi org.py
@@ -0,0 +1,16 @@
+# https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/7
+
+from machine import Pin, PWM
+from time import sleep
+
+pwm = PWM(Pin(17))
+
+pwm.freq(1000)
+
+while True:
+  for duty in range(65025):
+    pwm.duty_u16(duty)
+    sleep(0.0009)
+  for duty in range(65025, 0, -1):
+    pwm.duty_u16(duty)
+    sleep(0.0009)
--- pwm-test.py
+++ pwm-test.py
@@ -10,5 +10,5 @@ pwm15 = PWM(p15, freq=50, duty_u16=8192)  # create a PWM object on a pin
 while True: 
     for duty in range(0, 65535):
         pwm15.duty_u16(duty) # For ESP32
-        time.sleep(.00099)
+        time.sleep(.00099+.0000099)
 
